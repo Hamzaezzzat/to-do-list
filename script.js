1f75209da98bcaffed080db2f5b0f490e1a344ee
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-03-28 21:24:47 +0200

    Finished JS

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ allTasks.innerHTML += newTask;
 input.value = "";
 
 });
+
+// Change Icons!
+const heart = `<span class="icon icon-heart"></span>`;
+
+allTasks.addEventListener("click", (eo) =>{
+  eo.preventDefault();
+
+  switch (eo.target.className){
+    case "icon icon-star":
+      eo.target.style.opacity="1";
+      allTasks.prepend(eo.target.parentElement);
+      break;
+    case "icon icon-bin":
+      eo.target.parentElement.parentElement.remove();
+      break;
+    case "icon icon-sad":
+      eo.target.classList.add("icon-heart");
+      eo.target.classList.remove("icon-sad");
+      eo.target.parentElement.parentElement.querySelector("p").classList.add("checked");
+      break;
+    case "icon icon-heart":
+      eo.target.classList.add("icon-sad");
+      eo.target.classList.remove("icon-heart");
+      break;
+  }
+
+})
